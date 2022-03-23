d75cc77592e8fd1044e2d621dc9fa8b839156e1d
Louis Charnay <louis.charnay@orange.fr>
2022-03-23 08:35:30 +0100

    add loginPage

--- pokemon/src/pages/Login.vue
+++ pokemon/src/pages/Login.vue
@@ -1,9 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Login from './Login.vue';
+import Menu from '../components/Menu.vue';
+import FormLogin from '../components/FormLogin.vue';
 </script>
 
 <template>
-    <Login/>
+    <Menu/>
+    <FormLogin/>
 </template>
