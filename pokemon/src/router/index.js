6314d938f8d1989e399ed69d5f99d13d9513d3e0
Louis Charnay <louis.charnay@orange.fr>
2022-03-23 09:21:03 +0100

    corrections

--- pokemon/src/router/index.js
+++ pokemon/src/router/index.js
@@ -3,7 +3,8 @@ import Home from '../pages/Home.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/add', name: 'AddCard', component: () => import ('../pages/AddCard.vue')}
+    {path: '/add', name: 'AddCard', component: () => import ('../pages/AddCard.vue')},
+    {path: '/login', name: 'Login', component: () => import ('../pages/Login.vue')}
 ]
 
 const router = createRouter({
