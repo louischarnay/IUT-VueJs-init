3727825484074a2a45c04f1509cc7ae5dfce8bce
Pablo Guinard <guinardpablo@gmail.com>
2022-03-30 12:21:33 +0200

    add total price

--- pokemon/src/pages/Home.vue
+++ pokemon/src/pages/Home.vue
@@ -9,9 +9,19 @@ import { getAllPokemons } from "../services/api";
         mounted() {
             getAllPokemons().then( pokemons => this.pokemons = pokemons)
         },
+        computed: {
+            getFullPrice(){
+                let price = 0
+                this.pokemons.forEach(element => {
+                    price += element.price
+                })
+                return price
+            }
+        }
     } 
 </script>
 
 <template>
+    <p>Total price of the collection : {{getFullPrice}} €</p>
     <a v-for="item in pokemons" :href="'/' + item.id" ><img :src="item.image_path" :alt="item.name" /></a>
 </template>
